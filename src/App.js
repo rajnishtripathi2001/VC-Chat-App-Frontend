06a45e16744023b4c6b98c55ae4735fbdbca4a5b
Rajnish <rajnishtripathi2001@gmail.com>
2023-04-15 00:04:45 +0530

    url updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
 
-const socket = socketIO.connect('https://vc-chat-app-backend-production.up.railway.app/');
+const socket = socketIO.connect('http://vc-chat-app-backend-production.up.railway.app:4000');
+
+//http://localhost:4000
+//http://vc-chat-app-backend-production.up.railway.app/
 function App() {
   return (
     <BrowserRouter>
