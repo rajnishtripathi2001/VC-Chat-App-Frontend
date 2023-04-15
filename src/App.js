1c7da8c41ffaf690b9ea1a8815605cbfa56e5900
Rajnish <rajnishtripathi2001@gmail.com>
2023-04-15 11:24:48 +0530

    q

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home';
 import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
 
-const socket = socketIO.connect('https://vc-chat-app-backend-production.up.railway.app:4000');
+const socket = socketIO.connect('https://vc-chat-app-backend-production.up.railway.app/');
 
 //http://localhost:4000
 //https://vc-chat-app-backend-production.up.railway.app/
